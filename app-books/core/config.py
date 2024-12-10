fdf335b864629ba228dae5bd0a66f61c8118f14d
konstantin <kosta123139@gmail.com>
2024-12-10 10:37:13 +0300

    reed from env

--- app-books/core/config.py
+++ app-books/core/config.py
@@ -23,6 +23,12 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='FAST_API__'
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
